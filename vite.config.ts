673fb46235986d35d8cdd175210ca1a203fa28cc
catsinfo <62805522+helisinfo@users.noreply.github.com>
2025-07-10 18:21:00 +0200

    Ajout de la configuration du serveur dans vite.config.ts pour autoriser les connexions externes et définir les hôtes autorisés. Mise en place de la gestion du port client pour le HMR en fonction de l'environnement de production.

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,18 @@ export default defineConfig({
         ]
       : []),
   ],
+  server: {
+    host: true, // Autoriser les connexions externes
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "fleet.voluntis.space",
+      ".voluntis.space", // Autoriser tous les sous-domaines de voluntis.space
+    ],
+    hmr: {
+      clientPort: process.env.NODE_ENV === "production" ? 443 : undefined,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
